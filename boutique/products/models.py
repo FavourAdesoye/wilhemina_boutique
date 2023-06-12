6287e80f3452563a38edc72a0da698f433cf95eb
Favour Adesoye <adesoyeshubi@gmail.com>
2023-06-12 14:25:51 -0400

    second commit

--- boutique/products/models.py
+++ boutique/products/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+CATEGORY_CHOICES = (
+    ('clothes'),
+    ('bags'),
+    ('accessories')
+)
+
 
 class Clothes(models.Model):
     title = models.CharField(max_length=250)
     stock = models.IntegerField()
     price = models.FloatField()
-    cvr_url = models.CharField(max_length=2048)
+    product_image = models.ImageField(
+        upload_to='media/images/', default='blank')
 
-    def __str__(self):
-        return self.title
+    description = models.TextField(blank=True)
+
+
+def __str__(self):
+    return self.title
